765a61930323d41af36c12537245bfd7061d4138
Steve <stevelead@gmail.com>
2022-09-01 05:36:55 +1200

    add dynamic details to cart dropdown

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,27 +1,22 @@
 <script setup>
-const { siteName } = useAppConfig();
-const deskree = useDeskree();
-const loggedInUser = computed(() => deskree.loggedInUser.value);
+const { siteName } = useAppConfig()
+const deskree = useDeskree()
+const loggedInUser = computed(() => deskree.loggedInUser.value)
 const cartStore = useCartStore()
 </script>
 <template>
   <div class="shadow-md navbar bg-base-100">
     <div class="flex-1">
-      <NuxtLink class="text-xl normal-case btn btn-ghost" to="/">{{
-        siteName
-      }}</NuxtLink>
+      <NuxtLink class="text-xl normal-case btn btn-ghost" to="/">{{ siteName }}</NuxtLink>
     </div>
     <!-- Right Side -->
     <div class="flex-none">
       <div class="dropdown dropdown-end">
         <CartIcon :loading="false" :count="cartStore.count" />
-        <div
-          tabindex="0"
-          class="mt-3 shadow card card-compact dropdown-content w-52 bg-base-100"
-        >
+        <div tabindex="0" class="mt-3 shadow card card-compact dropdown-content w-52 bg-base-100">
           <div class="card-body">
-            <span class="text-lg font-bold">0 Items</span>
-            <span class="text-info">Subtotal: $0.00</span>
+            <span class="text-lg font-bold">{{ cartStore.count }} Items</span>
+            <span class="text-info">Subtotal: <ProductPrice :price="cartStore.subtotal" /></span>
             <div class="card-actions">
               <NuxtLink :to="{ name: 'cart' }">
                 <button class="btn btn-primary btn-block">View cart</button>
@@ -33,14 +28,8 @@ const cartStore = useCartStore()
 
       <!--Buttons for UN-logged In Users-->
       <div v-if="!loggedInUser">
-        <NuxtLink
-          to="/login"
-          class="ml-5 border-2 border-gray-100 btn btn-ghost btn-sm"
-          >Login</NuxtLink
-        >
-        <NuxtLink to="/register" class="ml-2 btn btn-primary btn-sm"
-          >Register</NuxtLink
-        >
+        <NuxtLink to="/login" class="ml-5 border-2 border-gray-100 btn btn-ghost btn-sm">Login</NuxtLink>
+        <NuxtLink to="/register" class="ml-2 btn btn-primary btn-sm">Register</NuxtLink>
       </div>
 
       <!--UI for logged In Users-->
@@ -48,10 +37,7 @@ const cartStore = useCartStore()
         <label tabindex="0" class="ml-5 btn btn-sm">
           <button>{{ loggedInUser.email }}</button>
         </label>
-        <ul
-          tabindex="0"
-          class="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-        >
+        <ul tabindex="0" class="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
           <li>
             <a class="justify-between">
               Profile
